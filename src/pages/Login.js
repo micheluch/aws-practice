fb2b1624351c7be3c640df41a2b3fa9780d622ec
Yevhen Voitiuk <yevhen.voitiuk447@myci.csuci.edu>
2023-03-09 19:14:04 -0800

    Added login functionality through AWS and tested!

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,18 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 const Login = () => {
-    return <h1>Le login</h1>;
+    return (
+        <Authenticator>
+            {({ signOut }) => (
+                <div>
+                    <h1>Welcome to the Premium Rice Fields</h1>
+                    <h3>You are authenticated. This is a premium page lmao</h3>
+                    <button onClick={signOut}>Sign TF Out</button>
+                </div>
+            )}
+        </Authenticator>
+    );
 };
 
 export default Login;
